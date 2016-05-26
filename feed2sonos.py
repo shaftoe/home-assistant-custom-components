d2fda30d6cb84012ca3a428ba3fea68db220483a
Alexander Fortin <alexander.fortin@gmail.com>
2016-05-26 06:39:17 +0200

    Enqueue every 'audio/mpeg' link available in the entry

--- feed2sonos.py
+++ feed2sonos.py
@@ -20,20 +20,19 @@ def setup(hass, config):
     """Setup the feed2sonos component."""
     def get_uri_from_data(entry):
         """Get mp3 link from feed entry data."""
-        for link in entry.links:
-            if link.get('type') == 'audio/mpeg':
-                return link.get('href')
+        return [link.get('href') for link in entry.links
+                if link.get('type') == 'audio/mpeg']
 
     def parse_event(event):
         """Parse event and add it to queue if has multimedia content."""
-        uri = get_uri_from_data(event.data)
-        if uri:
-            hass.services.call(MEDIA_PLAYER_DOMAIN,
-                               SERVICE_PLAY_MEDIA,
-                               {ATTR_MEDIA_CONTENT_ID: uri,
-                                ATTR_MEDIA_CONTENT_TYPE: 'audio/mpeg',
-                                ATTR_MEDIA_ENQUEUE: True})
-            _LOGGER.info('Added URI "%s" to queue', uri)
+        for uri in get_uri_from_data(event.data):
+            if uri:
+                hass.services.call(MEDIA_PLAYER_DOMAIN,
+                                   SERVICE_PLAY_MEDIA,
+                                   {ATTR_MEDIA_CONTENT_ID: uri,
+                                    ATTR_MEDIA_CONTENT_TYPE: 'audio/mpeg',
+                                    ATTR_MEDIA_ENQUEUE: True})
+                _LOGGER.info('Added URI "%s" to queue', uri)
 
     hass.bus.listen(EVENT_FEEDREADER, parse_event)
     return True
